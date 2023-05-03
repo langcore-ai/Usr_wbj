4db12275dd1701f5f139d4a4514c503b51f46a4d
王炳杰 <1253608269@qq.com>
2023-05-03 23:13:58 +0800

    1

--- mw/无网络.py
+++ mw/无网络.py
@@ -1,6 +1,17 @@
 from PySide6.QtWidgets import QApplication, QMessageBox
 
 app = QApplication()
+import socket
+
+def is_network_available():
+    try:
+        # 建立一个新的套接字连接到 Google DNS
+        s = socket.create_connection(("8.8.8.8", 53), 2)
+        s.close()
+        return True
+    except OSError:
+        pass
+    return False
 
 if not is_network_available():
     msg_box = QMessageBox()
